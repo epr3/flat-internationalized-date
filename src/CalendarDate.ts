d70f958d2e4675ee4dfd9b661ef109ed7540fba3
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-03-15 19:02:58 +0200

    chore: remove comments

--- src/CalendarDate.ts
+++ src/CalendarDate.ts
@@ -10,29 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
-import { IslamicUmalquraCalendar } from ".";
-import { calendars } from "./calendars";
+import { CALENDAR, calendars, IslamicUmalquraCalendar } from "./calendars";
 import { constrain, constrainTime } from "./manipulation";
-
-// function shiftArgs(args: any[]) {
-//     const calendar: Calendar = typeof args[0] === "object"
-//         ? args.shift()
-//         : new GregorianCalendar();
-
-//     let era: string;
-//     if (typeof args[0] === "string") {
-//         era = args.shift();
-//     } else {
-//         const eras = calendar.getEras();
-//         era = eras[eras.length - 1];
-//     }
-
-//     const year = args.shift();
-//     const month = args.shift();
-//     const day = args.shift();
-
-//     return [calendar, era, year, month, day];
-// }
 
 export function createCalendarDate({
   calendar,
@@ -41,21 +20,21 @@ export function createCalendarDate({
   month,
   day,
 }: {
-  calendar?: string;
+  calendar?: CALENDAR;
   era?: string;
   year: number;
   month: number;
   day: number;
 }): CalendarDate {
   let calendarObj: CalendarDate = {
-    calendar: calendar ?? "gregorian",
+    calendar: calendar ?? CALENDAR.GREGORIAN,
     era,
     year,
     month,
     day,
   };
 
-  if (calendar === "islamic-umalqura")
+  if (calendar === CALENDAR.ISLAMIC_UMALQURA)
     IslamicUmalquraCalendar.constructCalendar();
 
   if (!calendarObj.era) {
@@ -77,7 +56,7 @@ export function createCalendarDateTime({
   second,
   millisecond,
 }: {
-  calendar?: string;
+  calendar?: CALENDAR;
   era?: string;
   year: number;
   month: number;
@@ -117,7 +96,7 @@ export function createZonedDateTime({
   timezone,
   offset,
 }: {
-  calendar?: string;
+  calendar?: CALENDAR;
   era?: string;
   year: number;
   month: number;
@@ -174,7 +153,7 @@ export interface CalendarDate {
   // i.e. a ZonedDateTime should not be be passable to a parameter that expects CalendarDate.
   // If that behavior is desired, use the AnyCalendarDate interface instead.
   /** The calendar system associated with this date, e.g. Gregorian. */
-  readonly calendar: string;
+  readonly calendar: CALENDAR;
   /** The calendar era for this date, e.g. "BC" or "AD". */
   readonly era?: string;
   /** The year of this date within the era. */
@@ -187,68 +166,6 @@ export interface CalendarDate {
   readonly month: number;
   /** The day number within the month. */
   readonly day: number;
-
-  //     constructor(year: number, month: number, day: number);
-  //     constructor(era: string, year: number, month: number, day: number);
-  //     constructor(calendar: Calendar, year: number, month: number, day: number);
-  //     constructor(calendar: Calendar, era: string, year: number, month: number, day: number);
-  //     constructor(...args: any[]) {
-  //         const [calendar, era, year, month, day] = shiftArgs(args);
-  //         this.calendar = calendar;
-  //         this.era = era;
-  //         this.year = year;
-  //         this.month = month;
-  //         this.day = day;
-
-  //         constrain(this);
-  //     }
-
-  //     /** Returns a copy of this date. */
-  //     copy(): CalendarDate {
-  //         if (this.era) {
-  //             return new CalendarDate(this.calendar, this.era, this.year, this.month, this.day);
-  //         } else {
-  //             return new CalendarDate(this.calendar, this.year, this.month, this.day);
-  //         }
-  //     }
-
-  //     /** Returns a new `CalendarDate` with the given duration added to it. */
-  //     add(duration: DateDuration): CalendarDate {
-  //         return add(this, duration);
-  //     }
-
-  //     /** Returns a new `CalendarDate` with the given duration subtracted from it. */
-  //     subtract(duration: DateDuration): CalendarDate {
-  //         return subtract(this, duration);
-  //     }
-
-  //     /** Returns a new `CalendarDate` with the given fields set to the provided values. Other fields will be constrained accordingly. */
-  //     set(fields: DateFields): CalendarDate {
-  //         return set(this, fields);
-  //     }
-
-  //     /**
-  //    * Returns a new `CalendarDate` with the given field adjusted by a specified amount.
-  //    * When the resulting value reaches the limits of the field, it wraps around.
-  //    */
-  //     cycle(field: DateField, amount: number, options?: CycleOptions): CalendarDate {
-  //         return cycleDate(this, field, amount, options);
-  //     }
-
-  //     /** Converts the date to a native JavaScript Date object, with the time set to midnight in the given time zone. */
-  //     toDate(timeZone: string): Date {
-  //         return toDate(this, timeZone);
-  //     }
-
-  //     /** Converts the date to an ISO 8601 formatted string. */
-  //     toString(): string {
-  //         return dateToString(this);
-  //     }
-
-  //     /** Compares this date with another. A negative result indicates that this date is before the given one, and a positive date indicates that it is after. */
-  //     compare(b: AnyCalendarDate): number {
-  //         return compareDate(this, b);
-  //     }
 }
 
 /** A Time represents a clock time without any date components. */
@@ -262,57 +179,6 @@ export interface Time {
   readonly second: number;
   /** The millisecond in the second. */
   readonly millisecond: number;
-
-  //     constructor(
-  //         hour: number = 0,
-  //         minute: number = 0,
-  //         second: number = 0,
-  //         millisecond: number = 0
-  //     ) {
-  //         this.hour = hour;
-  //         this.minute = minute;
-  //         this.second = second;
-  //         this.millisecond = millisecond;
-  //         constrainTime(this);
-  //     }
-
-  //     /** Returns a copy of this time. */
-  //     copy(): Time {
-  //         return new Time(this.hour, this.minute, this.second, this.millisecond);
-  //     }
-
-  //     /** Returns a new `Time` with the given duration added to it. */
-  //     add(duration: TimeDuration) {
-  //         return addTime(this, duration);
-  //     }
-
-  //     /** Returns a new `Time` with the given duration subtracted from it. */
-  //     subtract(duration: TimeDuration) {
-  //         return subtractTime(this, duration);
-  //     }
-
-  //     /** Returns a new `Time` with the given fields set to the provided values. Other fields will be constrained accordingly. */
-  //     set(fields: TimeFields) {
-  //         return setTime(this, fields);
-  //     }
-
-  //     /**
-  //    * Returns a new `Time` with the given field adjusted by a specified amount.
-  //    * When the resulting value reaches the limits of the field, it wraps around.
-  //    */
-  //     cycle(field: TimeField, amount: number, options?: CycleTimeOptions) {
-  //         return cycleTime(this, field, amount, options);
-  //     }
-
-  //     /** Converts the time to an ISO 8601 formatted string. */
-  //     toString() {
-  //         return timeToString(this);
-  //     }
-
-  //     /** Compares this time with another. A negative result indicates that this time is before the given one, and a positive time indicates that it is after. */
-  //     compare(b: AnyTime) {
-  //         return compareTime(this, b);
-  //     }
 }
 
 /** A CalendarDateTime represents a date and time without a time zone, in a specific calendar system. */
@@ -325,85 +191,6 @@ export interface CalendarDateTime extends CalendarDate {
   readonly second: number;
   /** The millisecond in the second. */
   readonly millisecond: number;
-
-  //     constructor(year: number, month: number, day: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(era: string, year: number, month: number, day: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(calendar: Calendar, year: number, month: number, day: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(calendar: Calendar, era: string, year: number, month: number, day: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(...args: any[]) {
-  //         const [calendar, era, year, month, day] = shiftArgs(args);
-  //         this.calendar = calendar;
-  //         this.era = era;
-  //         this.year = year;
-  //         this.month = month;
-  //         this.day = day;
-  //         this.hour = args.shift() || 0;
-  //         this.minute = args.shift() || 0;
-  //         this.second = args.shift() || 0;
-  //         this.millisecond = args.shift() || 0;
-
-  //         constrain(this);
-  //     }
-
-  //     /** Returns a copy of this date. */
-  //     copy(): CalendarDateTime {
-  //         if (this.era) {
-  //             return new CalendarDateTime(this.calendar, this.era, this.year, this.month, this.day, this.hour, this.minute, this.second, this.millisecond);
-  //         } else {
-  //             return new CalendarDateTime(this.calendar, this.year, this.month, this.day, this.hour, this.minute, this.second, this.millisecond);
-  //         }
-  //     }
-
-  //     /** Returns a new `CalendarDateTime` with the given duration added to it. */
-  //     add(duration: DateTimeDuration): CalendarDateTime {
-  //         return add(this, duration);
-  //     }
-
-  //     /** Returns a new `CalendarDateTime` with the given duration subtracted from it. */
-  //     subtract(duration: DateTimeDuration): CalendarDateTime {
-  //         return subtract(this, duration);
-  //     }
-
-  //     /** Returns a new `CalendarDateTime` with the given fields set to the provided values. Other fields will be constrained accordingly. */
-  //     set(fields: DateFields & TimeFields): CalendarDateTime {
-  //         return set(setTime(this, fields), fields);
-  //     }
-
-  //     /**
-  //    * Returns a new `CalendarDateTime` with the given field adjusted by a specified amount.
-  //    * When the resulting value reaches the limits of the field, it wraps around.
-  //    */
-  //     cycle(field: DateField | TimeField, amount: number, options?: CycleTimeOptions): CalendarDateTime {
-  //         switch (field) {
-  //         case "era":
-  //         case "year":
-  //         case "month":
-  //         case "day":
-  //             return cycleDate(this, field, amount, options);
-  //         default:
-  //             return cycleTime(this, field, amount, options);
-  //         }
-  //     }
-
-  //     /** Converts the date to a native JavaScript Date object in the given time zone. */
-  //     toDate(timeZone: string, disambiguation?: Disambiguation): Date {
-  //         return toDate(this, timeZone, disambiguation);
-  //     }
-
-  //     /** Converts the date to an ISO 8601 formatted string. */
-  //     toString(): string {
-  //         return dateTimeToString(this);
-  //     }
-
-  //     /** Compares this date with another. A negative result indicates that this date is before the given one, and a positive date indicates that it is after. */
-  //     compare(b: CalendarDate | CalendarDateTime | ZonedDateTime): number {
-  //         const res = compareDate(this, b);
-  //         if (res === 0) {
-  //             return compareTime(this, toCalendarDateTime(b));
-  //         }
-
-  //         return res;
-  //     }
 }
 
 /** A ZonedDateTime represents a date and time in a specific time zone and calendar system. */
@@ -412,80 +199,4 @@ export interface ZonedDateTime extends CalendarDateTime {
   readonly timezone: string;
   /** The UTC offset for this time, in milliseconds. */
   readonly offset: number;
-
-  //     constructor(year: number, month: number, day: number, timeZone: string, offset: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(era: string, year: number, month: number, day: number, timeZone: string, offset: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(calendar: Calendar, year: number, month: number, day: number, timeZone: string, offset: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(calendar: Calendar, era: string, year: number, month: number, day: number, timeZone: string, offset: number, hour?: number, minute?: number, second?: number, millisecond?: number);
-  //     constructor(...args: any[]) {
-  //         const [calendar, era, year, month, day] = shiftArgs(args);
-  //         const timeZone = args.shift();
-  //         const offset = args.shift();
-  //         this.calendar = calendar;
-  //         this.era = era;
-  //         this.year = year;
-  //         this.month = month;
-  //         this.day = day;
-  //         this.timeZone = timeZone;
-  //         this.offset = offset;
-  //         this.hour = args.shift() || 0;
-  //         this.minute = args.shift() || 0;
-  //         this.second = args.shift() || 0;
-  //         this.millisecond = args.shift() || 0;
-
-  //         constrain(this);
-  //     }
-
-  //     /** Returns a copy of this date. */
-  //     copy(): ZonedDateTime {
-  //         if (this.era) {
-  //             return new ZonedDateTime(this.calendar, this.era, this.year, this.month, this.day, this.timeZone, this.offset, this.hour, this.minute, this.second, this.millisecond);
-  //         } else {
-  //             return new ZonedDateTime(this.calendar, this.year, this.month, this.day, this.timeZone, this.offset, this.hour, this.minute, this.second, this.millisecond);
-  //         }
-  //     }
-
-  //     /** Returns a new `ZonedDateTime` with the given duration added to it. */
-  //     add(duration: DateTimeDuration) {
-  //         return addZoned(this, duration);
-  //     }
-
-  //     /** Returns a new `ZonedDateTime` with the given duration subtracted from it. */
-  //     subtract(duration: DateTimeDuration) {
-  //         return subtractZoned(this, duration);
-  //     }
-
-  //     /** Returns a new `ZonedDateTime` with the given fields set to the provided values. Other fields will be constrained accordingly. */
-  //     set(fields: DateFields & TimeFields, disambiguation?: Disambiguation) {
-  //         return setZoned(this, fields, disambiguation);
-  //     }
-
-  //     /**
-  //    * Returns a new `ZonedDateTime` with the given field adjusted by a specified amount.
-  //    * When the resulting value reaches the limits of the field, it wraps around.
-  //    */
-  //     cycle(field: DateField | TimeField, amount: number, options?: CycleTimeOptions) {
-  //         return cycleZoned(this, field, amount, options);
-  //     }
-
-  //     /** Converts the date to a native JavaScript Date object. */
-  //     toDate() {
-  //         return zonedToDate(this);
-  //     }
-
-  //     /** Converts the date to an ISO 8601 formatted string, including the UTC offset and time zone identifier. */
-  //     toString() {
-  //         return zonedDateTimeToString(this);
-  //     }
-
-  //     /** Converts the date to an ISO 8601 formatted string in UTC. */
-  //     toAbsoluteString() {
-  //         return this.toDate().toISOString();
-  //     }
-
-  //     /** Compares this date with another. A negative result indicates that this date is before the given one, and a positive date indicates that it is after. */
-  //     compare(b: CalendarDate | CalendarDateTime | ZonedDateTime) {
-  //     // TODO: Is this a bad idea??
-  //         return this.toDate().getTime() - toZoned(b, this.timeZone).toDate().getTime();
-  //     }
 }
